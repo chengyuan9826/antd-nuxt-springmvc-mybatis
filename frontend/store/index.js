0e27be3d635a379e234eeb8447328c6edc13186e
左康生 <zhen0578@qq.com>
2019-06-17 11:55:47 +0800

    1.左侧菜单刷新状态保持 2.scss问题解决

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,16 +1,23 @@
 import api from '~/assets/js/common/api'
 // 一些全局的状态
-const state = {};
-const mutations = {};
+const state = {
+    selectedKey: '2'
+}
+const mutations = {
+    changeSelectedKey(state, key) {
+        state.selectedKey = key
+    }
+}
 const actions = {
-  nuxtServerInit ({ commit }, { req, $axios, redire }) {
-    // return $axios.post(api.user.info).then(({ data: { user } }) => {
-    //   // 放入登录用户信息
-    //   commit('user/SET_USER', user);
-    // });
-  },
+    nuxtServerInit({ commit }, { req, $axios, redire }) {
+        // return $axios.post(api.user.info).then(({ data: { user } }) => {
+        //   // 放入登录用户信息
+        //   commit('user/SET_USER', user);
+        // });
+    }
 }
 export default {
-  state: () => state,
-  mutations, actions
-};
+    state: () => state,
+    mutations,
+    actions
+}
